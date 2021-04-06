43820832bfabe62274f4587a14f34402725701c9
Francisco Meireles <franciscomeireles-10@hotmail.com>
2021-04-06 16:40:19 +0100

    Project added

---
+++ src/MeePoint/MeePoint/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MeePoint.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
