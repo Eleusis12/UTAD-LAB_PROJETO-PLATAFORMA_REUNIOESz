cbef5b2389dcd65a980c7971f43e0c2646cf7f4e
Francisco Meireles <franciscomeireles-10@hotmail.com>
2021-05-14 14:01:19 +0100

    Calendar already receives the correct data from meetings scheduled

--- src/MeePoint/MeePoint/Controllers/HomeController.cs
+++ src/MeePoint/MeePoint/Controllers/HomeController.cs
@@ -10,16 +10,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using MeePoint.Interfaces;
 using MeePoint.ViewModels;
+using MeePoint.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace MeePoint.Controllers
 {
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
+		private readonly UserManager<IdentityUser> _userManager;
 
-		public HomeController(ILogger<HomeController> logger)
+		private readonly ApplicationDbContext _context;
+
+		public HomeController(ILogger<HomeController> logger, ApplicationDbContext context, UserManager<IdentityUser> userManager)
 		{
 			_logger = logger;
+			_context = context;
+			_userManager = userManager;
 		}
 
 		[Authorize]
@@ -28,36 +36,47 @@ namespace MeePoint.Controllers
 			return View();
 		}
 
-		public ActionResult GetCalendarData()
+		public async Task<ActionResult> GetCalendarData()
 		{
+			// Obtém o utilizador que está autenticado
+			IdentityUser applicationUser = await _userManager.GetUserAsync(User);
+			string email = applicationUser?.Email; // will give the user's Email
+			var user = _context.RegisteredUsers.FirstOrDefault(m => m.Email == email);
+
+			// Queremos obter a lista de reuniões agendadas para o específico user
+			Dictionary<int, ICollection<Meeting>> meetings = _context.GroupMembers
+				.Include(m => m.Group)
+				.ThenInclude(m => m.Meetings)
+				.Where(m => m.UserID == user.RegisteredUserID)
+				.Select(x => new KeyValuePair<int, ICollection<Meeting>>(x.GroupID, x.Group.Meetings))
+				.ToDictionary(x => x.Key, x => x.Value);
+
+			// Inicializar lista de reuniões a enviar para a View
 			List<CalendarEvent> events = new List<CalendarEvent>();
-			CalendarEvent calendarEvent = new CalendarEvent()
+			var random = new Random();
+
+			foreach (KeyValuePair<int, ICollection<Meeting>> keyPair in meetings)
 			{
-				title = "Ola mundo",
-				description = "Reunião muito divertida",
-				id = 2,
-				backgroundColor = "000000",
-				borderColor = "ffffff",
-				allDay = "false",
-				end = DateTime.Now.AddDays(4).ToString("yyyy-MM-ddTHH:mm:ss"),
-				start = DateTime.Now.AddDays(4).ToString("yyyy-MM-ddTHH:mm:ss")
-			};
+				// Para cada grupo queremos enviar cores diferentes, de forma a poder distinguir as reuniões de acordo com o grupo
+				var color = String.Format("#{0:X6}", random.Next(0x1000000)); // = "#A197B9"
 
-			CalendarEvent calendarEvent2 = new CalendarEvent()
-			{
-				title = "asddsa",
-				description = "Reunião muito divertida",
-				id = 2,
-				backgroundColor = "000000",
-				borderColor = "ffffff",
-				allDay = "false",
-				end = DateTime.Now.AddDays(1).ToString("yyyy-MM-ddTHH:mm:ss"),
-				start = DateTime.Now.AddDays(1).ToString("yyyy-MM-ddTHH:mm:ss")
-			};
+				foreach (Meeting meeting in keyPair.Value)
+				{
+					events.Add(new CalendarEvent()
+					{
+						title = meeting.Group.Name,
+						description = meeting.Name,
+						id = meeting.MeetingID,
+						backgroundColor = color,
+						borderColor = color,
+						allDay = "false",
+						start = meeting.MeetingDate.ToString("yyyy-MM-ddTHH:mm:ss"),
+						end = (meeting.MeetingDate.AddMinutes(meeting.ExpectedDuration)).ToString("yyyy-MM-ddTHH:mm:ss")
+					});
+				}
+			}
 
-			events.Add(calendarEvent);
-			events.Add(calendarEvent2);
-
+			//return private Json(events);
 			return Json(events);
 		}
 
