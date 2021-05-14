d5bcef24c12fa005db746c3cfa2334886aec72e8
Francisco Meireles <franciscomeireles-10@hotmail.com>
2021-05-14 12:14:02 +0100

    Implemented demo calendar

--- src/MeePoint/MeePoint/Controllers/HomeController.cs
+++ src/MeePoint/MeePoint/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using MeePoint.Interfaces;
+using MeePoint.ViewModels;
 
 namespace MeePoint.Controllers
 {
@@ -25,6 +26,38 @@ namespace MeePoint.Controllers
 		public IActionResult MainPage()
 		{
 			return View();
+		}
+
+		public ActionResult GetCalendarData()
+		{
+			List<AvailibilityDto> data = new List<AvailibilityDto>();
+
+			//Statically create list and add data
+			AvailibilityDto infoObj1 = new AvailibilityDto();
+			infoObj1.Id = 1;
+			infoObj1.Title = "I am available";
+			infoObj1.Desc = "Description 1";
+			infoObj1.Start_Date = "2020-08-16 22:37:22.467";
+			infoObj1.End_Date = "2020-08-16 23:30:22.467";
+			data.Add(infoObj1);
+
+			AvailibilityDto infoObj2 = new AvailibilityDto();
+			infoObj2.Id = 2;
+			infoObj2.Title = "Available";
+			infoObj2.Desc = "Description 1";
+			infoObj2.Start_Date = "2020-08-17 10:00:22.467";
+			infoObj2.End_Date = "2020-08-17 11:00:22.467";
+			data.Add(infoObj2);
+
+			AvailibilityDto infoObj3 = new AvailibilityDto();
+			infoObj3.Id = 3;
+			infoObj3.Title = "Meeting";
+			infoObj3.Desc = "Description 1";
+			infoObj3.Start_Date = "2020-08-18 07:30:22.467";
+			infoObj3.End_Date = "2020-08-18 08:00:22.467";
+			data.Add(infoObj3);
+
+			return Json(data);
 		}
 
 		public IActionResult MeePoint()
