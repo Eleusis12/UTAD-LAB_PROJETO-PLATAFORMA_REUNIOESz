45354a996ee945c2e4c9a98ec3f788f29544e193
Francisco Meireles <franciscomeireles-10@hotmail.com>
2021-06-22 13:58:15 +0100

    Update Faulty Migration

--- src/MeePoint/MeePoint/Data/Migrations/20210505153930_RemoveUniqueConstraintGroups.cs
+++ src/MeePoint/MeePoint/Data/Migrations/20210505153930_RemoveUniqueConstraintGroups.cs
@@ -2,33 +2,36 @@
 
 namespace MeePoint.Data.Migrations
 {
-    public partial class RemoveUniqueConstraintGroups : Migration
-    {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
+	public partial class RemoveUniqueConstraintGroups : Migration
+	{
+		protected override void Up(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.DropIndex(
+				name: "IX_Groups_Name",
+				table: "Groups");
 
-            migrationBuilder.AlterColumn<string>(
-                name: "Name",
-                table: "Groups",
-                nullable: false,
-                oldClrType: typeof(string),
-                oldType: "nvarchar(450)");
-        }
+			migrationBuilder.AlterColumn<string>(
+				name: "Name",
+				table: "Groups",
+				nullable: false,
+				oldClrType: typeof(string),
+				oldType: "nvarchar(450)");
+		}
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AlterColumn<string>(
-                name: "Name",
-                table: "Groups",
-                type: "nvarchar(450)",
-                nullable: false,
-                oldClrType: typeof(string));
+		protected override void Down(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.AlterColumn<string>(
+				name: "Name",
+				table: "Groups",
+				type: "nvarchar(450)",
+				nullable: false,
+				oldClrType: typeof(string));
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Groups_Name",
-                table: "Groups",
-                column: "Name",
-                unique: true);
-        }
-    }
+			migrationBuilder.CreateIndex(
+				name: "IX_Groups_Name",
+				table: "Groups",
+				column: "Name",
+				unique: true);
+		}
+	}
 }
