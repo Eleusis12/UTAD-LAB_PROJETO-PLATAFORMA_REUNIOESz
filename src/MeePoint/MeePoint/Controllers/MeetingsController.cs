80e35ada4a2f2a2ded20bb570e4b93db7a0a8f03
Francisco Meireles <franciscomeireles-10@hotmail.com>
2021-06-25 12:01:10 +0100

    Deleted Useless Views

--- src/MeePoint/MeePoint/Controllers/MeetingsController.cs
+++ src/MeePoint/MeePoint/Controllers/MeetingsController.cs
@@ -39,11 +39,11 @@ namespace MeePoint.Controllers
 		}
 
 		// GET: Meetings
-		public async Task<IActionResult> Index()
-		{
-			var applicationDbContext = _context.Meetings.Include(m => m.Group);
-			return View(await applicationDbContext.ToListAsync());
-		}
+		//public async Task<IActionResult> Index()
+		//{
+		//	var applicationDbContext = _context.Meetings.Include(m => m.Group);
+		//	return View(await applicationDbContext.ToListAsync());
+		//}
 
 		// GET: Attendance
 		[HttpGet]
@@ -417,82 +417,82 @@ namespace MeePoint.Controllers
 			return View();
 		}
 
-		// GET: Meetings/Create
-		public IActionResult Create()
-		{
-			ViewData["GroupID"] = new SelectList(_context.Groups, "GroupID", "Name");
-			return View();
-		}
+		//// GET: Meetings/Create
+		//public IActionResult Create()
+		//{
+		//	ViewData["GroupID"] = new SelectList(_context.Groups, "GroupID", "Name");
+		//	return View();
+		//}
 
-		// POST: Meetings/Create
-		// To protect from overposting attacks, enable the specific properties you want to bind to, for
-		// more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> Create([Bind("MeetingID,GroupID,Quorum,MeetingDate")] Meeting meeting)
-		{
-			if (ModelState.IsValid)
-			{
-				_context.Add(meeting);
-				await _context.SaveChangesAsync();
-				return RedirectToAction(nameof(Index));
-			}
-			ViewData["GroupID"] = new SelectList(_context.Groups, "GroupID", "Name", meeting.GroupID);
-			return View(meeting);
-		}
+		//// POST: Meetings/Create
+		//// To protect from overposting attacks, enable the specific properties you want to bind to, for
+		//// more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		//[HttpPost]
+		//[ValidateAntiForgeryToken]
+		//public async Task<IActionResult> Create([Bind("MeetingID,GroupID,Quorum,MeetingDate")] Meeting meeting)
+		//{
+		//	if (ModelState.IsValid)
+		//	{
+		//		_context.Add(meeting);
+		//		await _context.SaveChangesAsync();
+		//		return RedirectToAction(nameof(Index));
+		//	}
+		//	ViewData["GroupID"] = new SelectList(_context.Groups, "GroupID", "Name", meeting.GroupID);
+		//	return View(meeting);
+		//}
 
-		// GET: Meetings/Edit/5
-		public async Task<IActionResult> Edit(int? id)
-		{
-			if (id == null)
-			{
-				return NotFound();
-			}
+		//// GET: Meetings/Edit/5
+		//public async Task<IActionResult> Edit(int? id)
+		//{
+		//	if (id == null)
+		//	{
+		//		return NotFound();
+		//	}
 
-			var meeting = await _context.Meetings.FindAsync(id);
-			if (meeting == null)
-			{
-				return NotFound();
-			}
-			ViewData["GroupID"] = new SelectList(_context.Groups, "GroupID", "Name", meeting.GroupID);
-			return View(meeting);
-		}
+		//	var meeting = await _context.Meetings.FindAsync(id);
+		//	if (meeting == null)
+		//	{
+		//		return NotFound();
+		//	}
+		//	ViewData["GroupID"] = new SelectList(_context.Groups, "GroupID", "Name", meeting.GroupID);
+		//	return View(meeting);
+		//}
 
-		// POST: Meetings/Edit/5
-		// To protect from overposting attacks, enable the specific properties you want to bind to, for
-		// more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> Edit(int id, [Bind("MeetingID,GroupID,Quorum,MeetingDate")] Meeting meeting)
-		{
-			if (id != meeting.MeetingID)
-			{
-				return NotFound();
-			}
+		//// POST: Meetings/Edit/5
+		//// To protect from overposting attacks, enable the specific properties you want to bind to, for
+		//// more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		//[HttpPost]
+		//[ValidateAntiForgeryToken]
+		//public async Task<IActionResult> Edit(int id, [Bind("MeetingID,GroupID,Quorum,MeetingDate")] Meeting meeting)
+		//{
+		//	if (id != meeting.MeetingID)
+		//	{
+		//		return NotFound();
+		//	}
 
-			if (ModelState.IsValid)
-			{
-				try
-				{
-					_context.Update(meeting);
-					await _context.SaveChangesAsync();
-				}
-				catch (DbUpdateConcurrencyException)
-				{
-					if (!MeetingExists(meeting.MeetingID))
-					{
-						return NotFound();
-					}
-					else
-					{
-						throw;
-					}
-				}
-				return RedirectToAction(nameof(Index));
-			}
-			ViewData["GroupID"] = new SelectList(_context.Groups, "GroupID", "Name", meeting.GroupID);
-			return View(meeting);
-		}
+		//	if (ModelState.IsValid)
+		//	{
+		//		try
+		//		{
+		//			_context.Update(meeting);
+		//			await _context.SaveChangesAsync();
+		//		}
+		//		catch (DbUpdateConcurrencyException)
+		//		{
+		//			if (!MeetingExists(meeting.MeetingID))
+		//			{
+		//				return NotFound();
+		//			}
+		//			else
+		//			{
+		//				throw;
+		//			}
+		//		}
+		//		return RedirectToAction(nameof(Index));
+		//	}
+		//	ViewData["GroupID"] = new SelectList(_context.Groups, "GroupID", "Name", meeting.GroupID);
+		//	return View(meeting);
+		//}
 
 		[HttpPost]
 		[Authorize]
